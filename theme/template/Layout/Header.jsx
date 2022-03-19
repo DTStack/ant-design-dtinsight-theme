8a8ed763cf4e2de532110c3efa9deb6c5b14deb0
xuxiaoqi <zhaoge@dtstack.com>
2022-03-19 20:18:20 +0800

    feat: upgrade antd to v3's top

--- theme/template/Layout/Header.jsx
+++ theme/template/Layout/Header.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import { Link } from 'bisheng/router';
 import { FormattedMessage } from 'react-intl';
 import classNames from 'classnames';
-import { Select, Row, Col, Radio } from 'antd';
+import { AutoComplete, Row, Col, Radio } from 'antd';
 import config from '../../../bisheng.config';
 import * as utils from '../utils';
 
-const Option = Select.Option;
+const Option = AutoComplete.Option;
 const searchEngine = 'Google';
 const searchLink = 'https://www.google.com/#q=site:ant.design+';
 
@@ -110,8 +110,9 @@ export default class Header extends React.Component {
                     </Col>
                     <Col lg={10} md={10} sm={24} xs={24}>
                         <div id="search-box" style={isDark ? { borderLeftColor: '#272A40' } : {}}>
-                            <Select
-                                mode="combobox"
+                            <AutoComplete
+                                // mode="combobox"
+                                // showSearch
                                 value={inputValue}
                                 dropdownClassName="component-select"
                                 placeholder={searchPlaceholder}
@@ -125,7 +126,7 @@ export default class Header extends React.Component {
                                     <FormattedMessage id="app.header.search" />
                                 </Option>
                                 {options}
-                            </Select>
+                            </AutoComplete>
                         </div>
                         {menuMode === 'horizontal' ? menu : null}
                     </Col>
