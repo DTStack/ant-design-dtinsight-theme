f89aa8d08b014b9a5608b101af0545db00cecf04
liuyi <liuyi@dtstack.com>
2022-10-17 16:09:19 +0800

    feat: notification icon filled

--- custom-icon/ExclamationCircleOutlined.js
+++ custom-icon/ExclamationCircleOutlined.js
@@ -1,4 +1,4 @@
 
-const ExclamationCircleOutlined = {"icon":{"tag":"svg","attrs":{"viewBox":"64 64 896 896","focusable":"false"},"children":[{"tag":"path","attrs":{"d":"M517.33 101.33c229.74 0 416 186.22 416 416 0 229.74-186.26 416-416 416-229.73 0-416-186.26-416-416 0-229.78 186.27-416 416-416zm0 64c-194.4 0-352 157.58-352 352 0 194.4 157.61 352 352 352 194.4 0 352-157.6 352-352 0-194.42-157.6-352-352-352zm-4.83 262.6a32 32 0 0132 32v256a32 32 0 11-64 0v-256c0-17.7 14.3-32 32-32zm-.5-149.05a42.66 42.66 0 110 85.33 42.66 42.66 0 110-85.33z"}}]},"name":"ExclamationCircleOutlined","theme":"outlined"};
+const ExclamationCircleOutlined = {"icon":{"tag":"svg","attrs":{"viewBox":"64 64 896 896","focusable":"false"},"children":[{"tag":"path","attrs":{"d":"M512 928C282.27 928 96 741.73 96 512 96 282.2 282.27 96 512 96s416 186.2 416 416c0 229.73-186.27 416-416 416zm0-558.69a41.6 41.6 0 10.02-83.18 41.6 41.6 0 00-.02 83.18zm-.05 368.53a31.15 31.15 0 0031.1-28.86l.1-2.34v-249.6a31.2 31.2 0 00-62.32-2.34l-.08 2.34v249.6a31.2 31.2 0 0031.2 31.2z"}}]},"name":"ExclamationCircleOutlined","theme":"outlined"};
 
 export default ExclamationCircleOutlined;
